5d4ae1a185051615ba0f6fac7f3c89000a4a77c9
logzinga <65839350+logzinga@users.noreply.github.com>
2023-11-15 10:17:48 +1100

    working pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,10 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    public static bool gameIsPaused = false;
+
+
+    public GameObject pauseMenu;
+
     void Start()
     {
        Debug.Log("PauseMenu.cs loaded!"); 
+
+       
+    }
+
+    void Update () {
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                if (gameIsPaused) {
+                    Resume();
+                } else {
+                    Pause();
+                }
+       }
+    }
+
+    public void Resume() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
     }
 
     public void QuitGame() {
