ff4a22b8a5bf4e4b824840b56e1e246b95333840
logzinga <65839350+logzinga@users.noreply.github.com>
2023-11-14 13:37:43 +1100

    realise this script isn't needed

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -7,12 +7,10 @@ public class FinishLine : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("FinishLine.cs Loaded!");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnCollisionEnter (Collision collision) {
+
     }
 }
